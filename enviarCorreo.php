1d7a9e7b14e37191baeb8215b361206252ed1442
Alvaro Begazo Carhuayo <alvaro15unmsm@gmail.com>
2020-02-26 19:21:04 -0500

    agregado un simple formulario con metodo post y un archivo php para una simple prueba, darle algo de aspecto al form mencionado antes, y comentado las ideas a realizar a futurom una de ellas es hacer un li con forma de circulo y que se despliegue verticalmente para dar en iniciar cesion, cerrar sesion o registrarse

---
+++ enviarCorreo.php
@@ -0,0 +1,12 @@
+<?php   
+
+    $nombres =  $_POST['nombres'];
+    $correo =  $_POST['correo'];
+    $contenido =  $_POST['contenido'];
+
+    // print "Codigo: {$f["cod_cli"]} ,  Nombre: {$f["nmb_cli"]}";
+    print "Codigo $nombres,  $correo,   $contenido";
+    print "Tu compra ha sido correcta $nombres con correo de $correo .";
+    // print "Codigo ";
+
+?>
