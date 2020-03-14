3c52b2bc46bcc44c6068cccbe2dd332bcafbc341
Alvaro Begazo Carhuayo <alvaro15unmsm@gmail.com>
2020-03-14 15:16:17 -0500

    sexta correccion del anterior commit

--- index.php
+++ index.php
@@ -10,7 +10,29 @@
 
 <body>
     <header>
-        
+        <a href="index.php"><img id="logoGammaRay" src="Imagenes/logo_Gamma_Ray.gif" alt="Logo de Gamma Ray" width="200" height="75"></a>
+
+        <nav id="menNav">
+            <ul>
+                <li id="listaMenu"><a href="index.php">Home</a></li> <!-- incluira la historia con pocas imagenes -->
+                <li id="listaMenu"><a href="routes/galeria.php">Galeria</a></li>
+                <li id="listaMenu"><a href="routes/conciertos.php">Conciertos</a></li>
+                <li id="listaMenu"><a href="routes/discografia.php">Discografia</a></li>
+                <li id="listaMenu"><a href="routes/letrasCanciones.php">Letra de Canciones</a></li>
+                <li id="listaMenu"><a href="routes/apoyo.php">Apoya al Grupo</a></li>
+                <li id="listaMenu"><a href="routes/login.php">Usuarios</a>
+                    <!-- <ul>
+                                <li id=""><a href="signin.php">Sign In</a></li>
+                                <li id=""><a href="signup.php">Sign Up</a></li>
+                            </ul> -->
+                </li>
+                <!-- 
+                            Este haremos para que e usuario se cree una cuenta o inicie sesion a ella, para poder hacer las compras, 
+                            ademas podra darle me gusta a las canciones que le gusta, tanto a los conciertos y las letras, etc.
+
+                        -->
+            </ul>
+        </nav>
     </header>
 
     <article>
@@ -100,8 +122,10 @@
 
     <div class="espacioFinal"></div>
 
+    <footer>
+        <p>Derechos Reservados a Gamma Ray</p>
+    </footer>
     
-
 </body>
 
 </html>
