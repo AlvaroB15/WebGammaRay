b4932101e578fc2173708a673b0b45cc3767200e
Alvaro Begazo Carhuayo <alvaro15unmsm@gmail.com>
2020-03-14 14:41:08 -0500

    cuarta correccion del anterior commit

--- index.php
+++ index.php
@@ -10,15 +10,7 @@
 
 <body>
     <header>
-
         <?php require('/empaquetado/cabecera.php') ?>
-    <!-- DOCUEMNTOS PARA MIRAR LA CONEXION DE POSTGRESQL CON PHP
-
-    https://parzibyte.me/blog/2019/06/17/conectar-postgresql-php-pdo-crud/
-
-    https://w3resource.com/PostgreSQL/PostgreSQL-with-php-a-through-introduction.php
-    
-    -->
     </header>
 
     <article>
